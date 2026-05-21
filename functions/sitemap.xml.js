a7a78285537871b96404eba3beaedd8534103746
Anabela Vijarora <anabela@lojarapidamz.com>
2026-05-21 14:31:10 +0200

    feat: Cloudflare Pages Function para sitemap.xml

---
+++ functions/sitemap.xml.js
@@ -0,0 +1,12 @@
+﻿export async function onRequest(context) {
+  const response = await fetch(
+    "https://bpzqdwpkwlwflrcwcrqp.supabase.co/functions/v1/sitemap"
+  )
+  const xml = await response.text()
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600",
+    }
+  })
+}
